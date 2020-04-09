dfaef7970cba1b6d0c44cf107c1414004e4227eb
Dmitriy <dmitriy.trochshenko@gmail.com>
2020-04-09 10:22:36 +0600

    add Flask Migrate and postgres support

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Server, Manager
 
-from api.app import create_app
+from api.app import create_app, db
 
 app = create_app(config_object='api.conf.DevelopmentConfig')
 manager = Manager(app)
 
+migrate = Migrate(app, db)
 
 manager.add_command("runserver", Server())
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
