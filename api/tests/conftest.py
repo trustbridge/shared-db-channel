a2467db2d6d0d5a11fae9107f702a2633574d94b
Dmitriy <dmitriy.trochshenko@gmail.com>
2020-04-20 14:20:01 +0600

    Add `?fields=` query param that takes comma separated list and return response schema based on requested fields Add webargs to requirements Update swagger docs

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
+from freezegun import freeze_time
 
 from api.app import create_app, db
 from api.conf import TestingConfig
+from api.models import Message
 
 
 @pytest.yield_fixture(scope='session')
@@ -21,3 +23,12 @@ def _db(app):
 def client(app):
     with app.app_context():
         yield app.test_client()
+
+
+@pytest.fixture
+@freeze_time('2020-04-07 14:21:22.123456')
+def message(db_session):
+    message = Message(id=42, payload={"sender": "AU"})
+    db_session.add(message)
+    db_session.commit()
+    return message
