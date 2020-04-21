8be2711851f3c95c0acd5cd24c7c7ebb8c7110a4
Dmitriy <dmitriy.trochshenko@gmail.com>
2020-04-22 05:15:12 +0600

    Add pie_tasks for building and running (#8)
    https://github.com/adamkerz/pie

---
+++ pie_tasks.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+from pie_docker_compose import *
+
+ROOT_DIR = Path('.').absolute()
+DOCKER_COMPOSE = DockerCompose('docker-compose.yml')
+DOCKER_COMPOSE_SHARED_DB = DockerCompose('docker-compose-shared-db.yml')
+
+
+@task
+def setup():
+    with cd(ROOT_DIR):
+        if ROOT_DIR.joinpath('.env').exists():
+            print("Exit: .env already exists")
+            exit(1)
+        print("Copying .env_sample to .env")
+        cmd('cp .env_sample .env')
+
+
+@task
+def build():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE.cmd('build')
+
+
+@task
+def start():
+    with cd(ROOT_DIR):
+        services = ['api', 'postgres']
+        DOCKER_COMPOSE.cmd('up', options=['-d'] + services)
+
+
+@task
+def start_db():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE.service('postgres').cmd('up', options=['-d'])
+        DOCKER_COMPOSE.service('api').cmd('run', options=['--rm'], container_cmd='python ./manage.py db upgrade')
+
+
+@task
+def start_channel_api():
+    with cd(ROOT_DIR):
+        with env({'COMPOSE_IGNORE_ORPHANS': 'True'}):
+            DOCKER_COMPOSE_SHARED_DB.service('api').cmd('up', options=['-d'])
+
+
+@task
+def stop():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE.cmd('down')
+
+
+@task
+def destroy():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE.cmd('down', options=['-v', '--rmi all'])
+
+
+@task
+def test():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE_SHARED_DB.service('tests').cmd('run', options=['--rm'])
+
+
+@task
+def generate_swagger():
+    with cd(ROOT_DIR):
+        DOCKER_COMPOSE_SHARED_DB.service('api').cmd(
+            'run', options=['--rm'], container_cmd='python ./manage.py generate_swagger'
+        )
