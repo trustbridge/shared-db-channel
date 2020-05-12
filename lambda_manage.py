963c618c524d9ac388f717ffd3432a2a7f11fe21
Michael Foley <roleyfoley@gmail.com>
2020-05-12 18:00:43 +1000

    feat: add db migration with lambda function (#16)
    * feat: add db migration with lambda function
    
    * refactor: remove different task modes and do one task
    
    * docs: add definition of lambda_manage
    
    * Tidy up (according to MY style preferences ;-) )
    
    Co-authored-by: Adam Kerz <gitHub@kerz.id.au>

---
+++ lambda_manage.py
@@ -0,0 +1,20 @@
+"""Provides management tasks which can be invoked using AWS Lambda.
+Each task you want to complete is configured with a lambda handler."""
+
+import os
+import logging
+
+from flask_migrate import upgrade
+
+from api.app import create_app
+from api.conf import AWSProductionConfig
+
+
+logger = logging.getLogger(__name__)
+app = create_app(AWSProductionConfig())
+
+
+def dbupgrade_handler(event, context):
+    revision = os.getenv('DB_REVISION', 'head')
+    with app.app_context():
+        upgrade(revision=revision)
