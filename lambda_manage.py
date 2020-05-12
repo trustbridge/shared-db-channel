4750b205066d8d1f41dc0eda3b91ab8e9854b87f
Adam Kerz <gitHub@kerz.id.au>
2020-05-12 13:56:43 +1000

    Tidy up (according to MY style preferences ;-) )

--- lambda_manage.py
+++ lambda_manage.py
@@ -1,3 +1,6 @@
+"""Provides management tasks which can be invoked using AWS Lambda.
+Each task you want to complete is configured with a lambda handler."""
+
 import os
 import logging
 
@@ -6,14 +9,12 @@ from flask_migrate import upgrade
 from api.app import create_app
 from api.conf import AWSProductionConfig
 
-'''Provides management tasks which can be invoked using AWS Lambda'''
-'''Each task you want to complete is configured with a lambda handler'''
 
 logger = logging.getLogger(__name__)
 app = create_app(AWSProductionConfig())
 
-def dbupgrade_handler(event, context):
 
+def dbupgrade_handler(event, context):
     revision = os.getenv('DB_REVISION', 'head')
     with app.app_context():
         upgrade(revision=revision)
