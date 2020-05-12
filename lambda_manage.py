eab2d7434de1970d4cca0b5018168612e2e730df
roleyfoley <roleyfoley@gmail.com>
2020-05-12 11:48:04 +1000

    refactor: remove different task modes and do one task

--- lambda_manage.py
+++ lambda_manage.py
@@ -1,33 +1,16 @@
 import os
 import logging
 
-from flask_migrate import upgrade, current
+from flask_migrate import upgrade
 
-from api.app import create_app, db
+from api.app import create_app
 from api.conf import AWSProductionConfig
 
 logger = logging.getLogger(__name__)
 app = create_app(AWSProductionConfig())
 
-def dbmgmt_handler(event, context):
+def dbupgrade_handler(event, context):
 
     revision = os.getenv('DB_REVISION', 'head')
-    task = 'upgrade'
-
-    if event.get('task'):
-        task = event.get('task')
-
-    if task == 'upgrade':
-        with app.app_context():
-            upgrade(revision=revision)
-
-    elif task == 'current':
-        with app.app_context():
-            current()
-
-    return {
-        'task' : task
-    }
-
-if __name__ == "__main__":
-    dbmgmt_handler({},{})
+    with app.app_context():
+        upgrade(revision=revision)
